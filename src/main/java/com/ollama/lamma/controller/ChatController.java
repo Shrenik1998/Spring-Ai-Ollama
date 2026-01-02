c6e46eb669d86886eb36ee83b26af9e6e6a56176
Shrenik Halyal <shrenikhalyal@Shreniks-MacBook-Air.local>
2026-01-02 21:37:57 +0530

    removed unused imports

--- src/main/java/com/ollama/lamma/controller/ChatController.java
+++ src/main/java/com/ollama/lamma/controller/ChatController.java
@@ -1,7 +1,6 @@
 package com.ollama.lamma.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
