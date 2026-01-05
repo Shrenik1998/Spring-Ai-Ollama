cdd3e0246e3a923eddde3da1d2fba0d72c1b09e8
Shrenik Halyal <shrenikhalyal@Shreniks-MacBook-Air.local>
2026-01-05 08:31:54 +0530

    chat memory

---
+++ src/main/java/com/ollama/lamma/config/ChatClientForMemoryConfig.java
@@ -0,0 +1,27 @@
+package com.ollama.lamma.config;
+
+import com.ollama.lamma.advisors.TokenUsageAuditAdvisor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ChatClientForMemoryConfig {
+
+    @Bean("chatMemoryChatClient")
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+        return chatClientBuilder
+                .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor))
+                .build();
+    }
+}
